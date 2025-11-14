20784022f5f6ebc9c26a56a4b164db896b917c3f
Simulasi-Ekonomi <butetnadia@gmail.com>
2025-11-14 07:06:28 +0700

    Update main_menu.gd

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,7 +1,7 @@
-extends Control
+extends Button
 
 func _ready():
-	print("MainMenu ready")
+	pass
 
-func _on_start_pressed():
-	get_tree().change_scene_to_file("res://Scenes/GameWorld.tscn")
+func _pressed():
+    get_tree().change_scene_to_file("res://Scenes/GameWorld.tscn")
