5e782dcc29a2425efd4ac7507e9d605a444c01cd
Simulasi-Ekonomi <butetnadia@gmail.com>
2025-11-15 19:01:05 +0700

    Update GodotApp.java

--- android/src/org/godotengine/godot/GodotApp.java
+++ android/src/org/godotengine/godot/GodotApp.java
@@ -1,1 +1,8 @@
-Godot App placeholder
+package org.godotengine.godot;
+
+// Minimal placeholder class for Gradle
+public class GodotApp {
+    public static void init() {
+        // placeholder for template
+    }
+}
